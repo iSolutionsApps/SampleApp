5c1ee51035966ea252086fc9e52b982d4b053c22
ndot <ndot@AnandiOS.local>
2015-09-25 13:04:58 +0530

    First Commit

---
+++ SampleSwift/SampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSwift
+//
+//  Created by ndot on 9/25/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
